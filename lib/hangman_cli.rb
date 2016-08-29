a6566a652871c595af66020ba8ddd4539da6327b
randomcodenz <neal.blomfield@gmail.com>
2016-08-30 10:04:14 +1200

    Improved option processing
    Added missing option validation
    Added "nice" error message display for invalid options and missing options

--- lib/hangman_cli.rb
+++ lib/hangman_cli.rb
@@ -4,17 +4,22 @@ require 'hangman_cli/game'
 
 module HangmanCLI
   def HangmanCLI.parse_options(args)
-    options = { :lives => nil, :word => 'Powershop' }
+    options = { :lives => nil, :word => nil, :use_dictionary => false }
 
     parser = OptionParser.new do |opts|
-      opts.banner = 'Usage: hangman [options]'
+      opts.banner = "Usage:\n\thangman -w <word> [-l <lives>]\n\thangman -d [-l <lives>]\nOptions:"
 
       opts.on('-l', '--lives LIVES', 'Number of lives to start the game with') do |lives|
         options[:lives] = lives
       end
 
-      opts.on('-w', '--word WORD', 'The word to be guessed') do |word|
+      opts.on('-w', '--word WORD', 'The word to be guessed (overrides -d)') do |word|
         options[:word] = word
+        options[:use_dictionary] = false
+      end
+
+      opts.on('-d' '--dictionary', 'Use the system words file from /usr/share/dict/words') do
+          options[:use_dictionary] = true unless options[:word]
       end
 
       opts.on('-h', '--help', 'Prints this help') do
@@ -23,8 +28,19 @@ module HangmanCLI
       end
     end
 
-    parser.parse(args)
-    options
+    begin
+      parser.parse(args)
+
+      unless options[:word] || options[:use_dictionary]
+        raise OptionParser::MissingArgument.new('Must specify a word or use dictionary.')
+      end
+
+      options
+    rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+      puts $!.to_s
+      puts parser
+      exit
+    end
   end
 
   def HangmanCLI.play_hangman(input, output, error, options)
