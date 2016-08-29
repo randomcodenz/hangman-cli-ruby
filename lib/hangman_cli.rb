c23b6fda778d2f6420b3bf0350b0b889f66f17a0
randomcodenz <neal.blomfield@gmail.com>
2016-08-29 12:19:07 +1200

    Added a shell script to run the game
    Added a top-level module method to create and run the game
    Added a shell script to invoke the module method passing the appropriate string buffers for a console game

--- lib/hangman_cli.rb
+++ lib/hangman_cli.rb
@@ -1,5 +1,11 @@
-require "hangman_cli/version"
+require 'hangman_cli/version'
+require 'hangman_cli/ui'
+require 'hangman_cli/game'
 
-module HangmanCli
-  # Your code goes here...
+module HangmanCLI
+  def HangmanCLI.play_hangman(input, output, error)
+    ui = UI.new(input, output, error)
+    game = Game.new(ui, 5, 'Powershop')
+    game.start
+  end
 end
